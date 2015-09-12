8a759c2a080a21cbc4a86fc203d6a5a1288a3c3e
kostas liakopoulos <liakopoulos@hotmail.com>
2015-09-12 12:45:18 +0300

    added large text

--- app/src/main/java/com/wizdom/kostas/myapplication/Main2Activity.java
+++ app/src/main/java/com/wizdom/kostas/myapplication/Main2Activity.java
@@ -1,33 +1,22 @@
 package com.wizdom.kostas.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
-
-    public void SendMessage(View view)
-    {
-        Intent intent = new Intent(this,Main2Activity.class);
-        startActivity(intent);
-    }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 

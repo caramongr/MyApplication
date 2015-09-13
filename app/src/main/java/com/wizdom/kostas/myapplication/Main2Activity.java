672f14ef3780ee4c13c55e62b67ae40a40ad2324
kostas liakopoulos <liakopoulos@hotmail.com>
2015-09-13 22:32:53 +0300

    added toast and action bar

--- app/src/main/java/com/wizdom/kostas/myapplication/Main2Activity.java
+++ app/src/main/java/com/wizdom/kostas/myapplication/Main2Activity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
+
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.Extra_message);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
--- app/src/main/java/com/wizdom/kostas/myapplication/MainActivity.java
+++ app/src/main/java/com/wizdom/kostas/myapplication/MainActivity.java
@@ -1,24 +1,32 @@
 package com.wizdom.kostas.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
+public final static String Extra_message = "tade";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void SendMessage(View view)
     {
         Intent intent = new Intent(this,Main2Activity.class);
+        EditText mytext = (EditText)findViewById(R.id.editText);
+        String message = mytext.getText().toString();
+        intent.putExtra(Extra_message, message);
         startActivity(intent);
     }
 
@@ -36,13 +44,25 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void openSettings() {
+    }
+
+    private void openSearch() {
+        Toast.makeText(getApplicationContext(), "msg msg", Toast.LENGTH_SHORT).show();
     }
 }
